ddc816c426757415b2928708070e109ea6ccaef5
FormAToz <kycok2@yandex.ru>
2020-05-30 21:08:53 +0300

    Suffix tree realised.

--- src/main/java/suffix_tree/Node.java
+++ src/main/java/suffix_tree/Node.java
@@ -3,31 +3,34 @@ package suffix_tree;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Node
-{
+public class Node {
     private String fragment;
-    private ArrayList<Integer> nextNodes;
+    private List<Node> nextNodes;
     private int position;
 
-    public Node(String fragment, int position)
-    {
+    public Node(String fragment, int position) {
         this.fragment = fragment;
         nextNodes = new ArrayList<>();
         this.position = position;
     }
 
-    public String getFragment()
-    {
+    public void setFragment(String fragment) {
+        this.fragment = fragment;
+    }
+
+    public String getFragment() {
         return fragment;
     }
 
-    public int getPosition()
-    {
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public int getPosition() {
         return position;
     }
 
-    public List<Integer> getNextNodes()
-    {
+    public List<Node> getNextNodes() {
         return nextNodes;
     }
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,6 +4,7 @@ import double_linked_list.DoubleLinkedList;
 import junit.framework.TestCase;
 import single_linked_list.ListItem;
 import single_linked_list.SingleLinkedList;
+import suffix_tree.SuffixTree;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,6 +15,7 @@ public class Test extends TestCase {
     private DoubleLinkedList doubleLL_headInsert;
     private DoubleLinkedList doubleLL_tailInsert;
     private BinaryTree bTree;
+    private SuffixTree sfxTree;
     private List expected;
     private List actual;
     private String expString;
@@ -21,11 +23,13 @@ public class Test extends TestCase {
 
     @Override
     protected void setUp() throws Exception {
+        // single_linked_list
         singleLinkedList = new SingleLinkedList();
         singleLinkedList.push(new ListItem("One"));
         singleLinkedList.push(new ListItem("Two"));
         singleLinkedList.push(new ListItem("Three"));
 
+        // double_linked_list
         doubleLL_headInsert = new DoubleLinkedList();
         doubleLL_headInsert.addToHead(new double_linked_list.ListItem("1"));
         doubleLL_headInsert.addToHead(new double_linked_list.ListItem("2"));
@@ -36,7 +40,7 @@ public class Test extends TestCase {
         doubleLL_tailInsert.addToTail(new double_linked_list.ListItem("2"));
         doubleLL_tailInsert.addToTail(new double_linked_list.ListItem("3"));
 
-        /* sample tree:
+        /* binary_tree:
         *       C
         *     /   \
         *   A      C
@@ -60,34 +64,39 @@ public class Test extends TestCase {
         singleLinkedList.removeLast();
         expected = Arrays.asList("Three", "Two");
         actual = singleLinkedList.getAllItems();
-        assertTrue("Lists aren`t equals", expected.equals(actual));
+        assertTrue("Verify failed! Expected: " + expected.toString() + "\n Actual: " + actual.toString(),
+                expected.equals(actual));
     }
 
     // tests double_linked_list
     public void testInsertToHead() {
         expected = Arrays.asList("3", "2", "1");
         actual = doubleLL_headInsert.getAllElemForward();
-        assertTrue("Lists aren`t equals", expected.equals(actual));
+        assertTrue("Verify failed! Expected: " + expected.toString() + "\n Actual: " + actual.toString(),
+                expected.equals(actual));
     }
 
     public void testInsertToTail() {
         expected = Arrays.asList("1", "2", "3");
         actual = doubleLL_tailInsert.getAllElemForward();
-        assertTrue("Lists aren`t equals", expected.equals(actual));
+        assertTrue("Verify failed! Expected: " + expected.toString() + "\n Actual: " + actual.toString(),
+                expected.equals(actual));
     }
 
     public void testRemoveHeadElement() {
         doubleLL_tailInsert.removeHeadElement();
         expected = Arrays.asList("2", "3");
         actual = doubleLL_tailInsert.getAllElemForward();
-        assertTrue("Lists aren`t equals", expected.equals(actual));
+        assertTrue("Verify failed! Expected: " + expected.toString() + "\n Actual: " + actual.toString(),
+                expected.equals(actual));
     }
 
     public void testRemoveTailElement() {
         doubleLL_tailInsert.removeTailElement();
         expected = Arrays.asList("1", "2");
         actual = doubleLL_tailInsert.getAllElemForward();
-        assertTrue("Lists aren`t equals", expected.equals(actual));
+        assertTrue("Verify failed! Expected: " + expected.toString() + "\n Actual: " + actual.toString(),
+                expected.equals(actual));
     }
 
     public void testPopTailElement() {
@@ -112,12 +121,21 @@ public class Test extends TestCase {
     }
 
     public void testFindNodes() {
-        actual = Arrays.asList("C", "C", "C");
-        expected = bTree.searchNodes("C")
+        expected = Arrays.asList("C", "C", "C");
+        actual = bTree.searchNodes("C")
                 .stream()
                 .map(el -> el.toString())
                 .collect(Collectors.toList());
-        expected.forEach(System.out::println);
-        assertTrue("Lists aren`t equals", expected.equals(actual));
+        assertTrue("Verify failed! Expected: " + expected.toString() + "\n Actual: " + actual.toString(),
+                expected.equals(actual));
+    }
+
+    // tests suffix_tree
+    public void testSuffixTreeSearch() {
+        sfxTree = new SuffixTree("arttppasdsda"); //positions from 0 to n
+        expected = Arrays.asList(0, 6, 11);
+        actual = sfxTree.search("a");
+        assertTrue("Verify failed! Expected: " + expected.toString() + "\n Actual: " + actual.toString(),
+                expected.equals(actual));
     }
 }
